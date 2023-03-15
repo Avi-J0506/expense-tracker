df5447030fe1e45e3502358a257bd4953200e2bb
Avi-J0506 <92037313+Avi-J0506@users.noreply.github.com>
2023-03-15 23:49:17 +0530

    Expense Tracker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './Components/header';
+import { Balance } from './Components/balance';
+import { IncomeExpenses } from './Components/incomeexpenses';
+import { AddTransaction } from './Components/AddTransaction';
+import { Transactionlist } from './Components/transactionlist';
+
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <Transactionlist />
+        <AddTransaction/>
+      </div>
+    </GlobalProvider>
   );
 }
 
